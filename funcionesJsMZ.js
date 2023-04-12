57d9931fa8a108ecc23dce3991cd4d181c47f571
pepe <martazambranocruz@gmail.com>
2023-04-12 14:20:07 +0200

    funciones hasta el ejercicio 6

--- funcionesJsMZ.js
+++ funcionesJsMZ.js
@@ -2,6 +2,7 @@
 1. Escribe una función llamada saludar que acepte un argumento nombre y muestre en consola
 un saludo personalizado, por ejemplo, "¡Hola, Juan!".
  */
+
 function saludo (nombre){
     console.log(`¡Hola ${nombre} !`);
 }
@@ -10,10 +11,10 @@ console.log(saludo("Juan"));
 saludo("Patricia");
 
 //Otro ejemplo
-let username = "marta";
+let username = "martaz";
 function isValidUserName(username){
     
-    if (username.length > 5 && username.length<10){
+    if (username.length > 5 && username.length < 10){
         console.log("Clave registrada");
     } else {
         console.log("Clave no válida");
@@ -28,6 +29,7 @@ devuelva el resultado
 
 function sumar(a,b){
    let resultado = a+b;
+   return resultado;
 }
 console.log(sumar(3,4));
 
@@ -36,3 +38,50 @@ una variable
 1. Escribe una función anónima que acepte un número y devuelva su cuadrado. Asigna esta
 función a una variable llamada calcularCuadrado
 */
+const calcularCuadrado = function(numero){
+    return numero*numero;
+} 
+console.log (`El cuadrado es : ${calcularCuadrado(5)}`);
+
+
+/*4. Crear una función de flecha
+1. Escribe una función de flecha llamada multiplicar que acepte dos argumentos a y b, y devuelva
+su producto
+*/
+const multiplicar = (a,b) => a*b;
+console.log(`El resultado de multiplicar 5*10 es ${multiplicar(10,5)}`);
+
+/*5. Crear una función que utilice otra función
+1. Escribe una función llamada operacion que acepte tres argumentos: a, b y una función
+operador. La función operacion debe ejecutar la función operador con los argumentos a y b y
+devolver el resultado
+*/
+
+function operacion (num1, num2, funcionOperacion){
+    let result = funcionOperacion(num1,num2);
+    console.log(`El resultado de la operacion de ${num1} y ${num2} = ${result}`);
+}
+operacion(3,4,(num1, num2)=> num1 + num2);
+operacion(3,4,(num1, num2)=> num1 * num2);
+operacion(3,4,(num1, num2)=> num1 - num2);
+
+let suma = (j,k) => j + k;
+operacion(10,800, suma);
+
+/*6. Crear una función que devuelve otra
+función
+1. Escribe una función llamada crearMultiplicador que acepte un argumento factor. La función
+debe devolver otra función que acepte un número y devuelva el resultado de multiplicar ese
+número por el factor dado
+ */
+
+function crearMultiplicador(factor){
+    return function(numero){
+        return numero * factor;
+    }
+}
+let multiplicarPor5 = crearMultiplicador(5);
+console.log(multiplicarPor5(1));
+
+let multiplicarPor7 = crearMultiplicador(7);
+console.log(multiplicarPor7(2));
