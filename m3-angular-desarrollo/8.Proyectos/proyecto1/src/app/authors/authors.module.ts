0544fc2e8c0bf0a955ec2e4b5dbdc7f6ca0140cf
pepe <martazambranocruz@gmail.com>
2023-06-13 10:42:39 +0200

    Proyecto 1 books detail author

--- m3-angular-desarrollo/8.Proyectos/proyecto1/src/app/authors/authors.module.ts
+++ m3-angular-desarrollo/8.Proyectos/proyecto1/src/app/authors/authors.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthorsRoutingModule } from './authors-routing.module';
-
+import { AuthorDetailComponent } from './author-detail/author-detail.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthorDetailComponent
+  ],
   imports: [
     CommonModule,
-    AuthorsRoutingModule
+    AuthorsRoutingModule,
+    MatGridListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatListModule
   ]
 })
 export class AuthorsModule { }
