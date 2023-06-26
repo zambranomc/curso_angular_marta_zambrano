ca8f692a8ddcc3c0873a93d9e1f3ca34816f40ee
pepe <martazambranocruz@gmail.com>
2023-06-26 13:04:54 +0200

    backende06 app module

--- m5-backend/backend06/src/app.module.ts
+++ m5-backend/backend06/src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthorsModule } from './authors/authors.module';
+import { EditorialsModule } from './editorials/editorials.module';
+import { LocationsModule } from './locations/locations.module';
 
 
 @Module({
-  imports: [DatabaseModule, BooksModule, AuthorsModule],
+  imports: [DatabaseModule, BooksModule, AuthorsModule, EditorialsModule, LocationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
