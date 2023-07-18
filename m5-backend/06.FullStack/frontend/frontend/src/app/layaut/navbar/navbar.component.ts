b3af643420a09f35c4fd99705f8b168da0c97f0a
pepe <martazambranocruz@gmail.com>
2023-07-18 14:19:48 +0200

    FullStack shared constantes y navbar

--- m5-backend/06.FullStack/frontend/frontend/src/app/layaut/navbar/navbar.component.ts
+++ m5-backend/06.FullStack/frontend/frontend/src/app/layaut/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
@@ -6,8 +6,20 @@ import { AuthService } from 'src/app/auth/auth.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  isLoggedIn = false;
+  isAdmin = false;
+  isOwner = false;
 
   constructor(public authService: AuthService){}
+
+
+  ngOnInit(): void {
+    this.authService.isLoggedIn.subscribe(loggedIn => this.isLoggedIn = loggedIn);
+    this.authService.isAdmin.subscribe(admin => this.isAdmin = admin);
+    this.authService.isOwner.subscribe(owner => this.isOwner = owner);
+    }
+  
 
 }
---
+++ m5-backend/06.FullStack/frontend/frontend/src/app/shared/constants.ts
@@ -0,0 +1,2 @@
+export const BASE_URL: string = 'http://localhost:3000';
+export const TOKEN: string = 'jwt_token';
